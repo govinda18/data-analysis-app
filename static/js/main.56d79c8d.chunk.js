4e117679d692d28b57a7ba66b0d3939caf2d425e
govinda18 <govinda.totla.cse16@itbhu.ac.in>
2021-05-14 11:26:38 +0530

    Updates

--- static/js/main.56d79c8d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdata-analysis-app"]=this["webpackJsonpdata-analysis-app"]||[]).push([[0],{9:function(a,s,t){"use strict";t.r(s);t(0);var e=t(3),n=t.n(e),p=t(1),c=function(a){return Object(p.jsx)("h1",{children:"Samasdasdple App"})};n.a.render(Object(p.jsx)(c,{}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.56d79c8d.chunk.js.map
